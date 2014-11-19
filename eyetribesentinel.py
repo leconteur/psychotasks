1b98c27efe3d8a69084e2f19588d9c0dce982588
leconteur <olivier.gagnon737@gmail.com>
2014-11-19 13:51:17 -0500

    Solves the bug were the sentinels disconnected from the eyetribe server and created an exception.

--- eyetribesentinel.py
+++ eyetribesentinel.py
@@ -4,14 +4,23 @@ import json
 class EyetribeSentinel(object):
     def __init__(self, port=6555):
         self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect(("localhost", self.port))
         self.msg = json.dumps({"category":"tracker","request":"get",
                                "values":["trackerstate"]})
+        self.connect()
+        
+    def connect(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect(("localhost", self.port))
+
 
     def checkValid(self):
-        self.sock.send(self.msg)
-        msg = json.loads(self.sock.recv(1024))
+        try:
+            self.sock.send(self.msg)
+            msg = json.loads(self.sock.recv(1024))
+        except socket.error as e:
+            self.connect()
+            self.sock.send(self.msg)
+            msg = json.loads(self.sock.recv(1024))            
         try:
             return msg['values']['trackerstate'] == 0
         except KeyError:
@@ -24,7 +33,14 @@ class EyetribeSentinel(object):
         return "TheEyeTribe eyetracker"
         
 if __name__ == "__main__":
+    import time
     s = EyetribeSentinel()
     valid = s.checkValid()
     print "Valid : {}".format(valid)
+    valid = s.checkValid()
+    print "Valid : {}".format(valid)
+    for _ in range(15):
+        time.sleep(15)
+        valid = s.checkValid()
+        print "Valid : {}".format(valid)
     s.close()
